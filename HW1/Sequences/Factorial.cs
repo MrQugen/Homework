b45dc00f089f05f046d19ea508cc58314e74a730
MrQugen <mrqugen@gmail.com>
2022-07-29 12:53:19 +0300

    Add Factorial.cs and Fibonacci.cs

---
+++ HW1/Sequences/Factorial.cs
@@ -0,0 +1,14 @@
+﻿namespace HW1.Sequences;
+
+public static class Factorial
+{
+    public static int Calculate(int number)
+    {
+        if (number < 0)
+        {
+            throw new ArgumentException("Number cannot be negative");
+        }
+        
+        return Enumerable.Range(1, number).Aggregate(1, (f, i) => f * i);
+    }
+}
